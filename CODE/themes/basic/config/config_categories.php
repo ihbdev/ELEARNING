6d4edc54e81f5751ee593342db7c1579945f28eb
unknown <user@user-DK.(none)>
2012-06-14 11:34:59 +0700

    no

--- CODE/themes/basic/config/config_categories.php
+++ CODE/themes/basic/config/config_categories.php
@@ -5,6 +5,13 @@ return array(
 			'max_rank'=>3,
 			'code'=>'news',
 			'class'=>'News',
-			'label'=>'Danh mục tin tức-sự kiện'	
+			'label'=>'Danh mục tin tức-sự kiện',
+		),
+		Category::TYPE_ITEST=>array(
+			'form'=>'_form',
+			'max_rank'=>1,
+			'code'=>'itest',
+			'class'=>'ITest',
+			'label'=>'List Language',
 		),
 	);
